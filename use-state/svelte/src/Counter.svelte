a6cd361b74acc16b2e8de97a46c469121108d277
Josh Nussbaum <joshnuss@gmail.com>
2020-10-11 20:31:08 -0400

    Adds svelte example of useState()

---
+++ use-state/svelte/src/Counter.svelte
@@ -0,0 +1,8 @@
+<script>
+	export let count = 0
+</script>
+
+Count: {count}
+<button on:click={() => count = 0}>Reset</button>
+<button on:click={() => count -= 1}>-</button>
+<button on:click={() => count += 1}>+</button>

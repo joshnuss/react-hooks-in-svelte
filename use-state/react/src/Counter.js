cbd5989b099ceae7648921841e02f33cfb367320
Josh Nussbaum <joshnuss@gmail.com>
2020-10-11 20:37:37 -0400

    Adds react example of useState()

---
+++ use-state/react/src/Counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+    </>
+  );
+}

79afc331de9b8547c92ee569ae31aa164caeec92
Josh Nussbaum <joshnuss@gmail.com>
2021-11-05 20:52:33 -0400

    Simplify store example

--- use-reducer/svelte/src/Counter.svelte
+++ use-reducer/svelte/src/Counter.svelte
@@ -1,17 +1,17 @@
 <script>
   import { writable } from 'svelte/store'
-  const initialValue = {count: 0}
 
-  const store = writable(initialValue)
+  const initialValue = 0
+  const count = writable(initialValue)
 
-  store.increment = () => {
-    store.update(state => ({count: state.count + 1}))
+  count.increment = () => {
+    count.update(state => state + 1)
   }
-  store.decrement = () => {
-    store.update(state => ({count: state.count - 1}))
+  count.decrement = () => {
+    count.update(state => state - 1)
   }
 </script>
 
-Count: {$store.count}
-<button on:click={() => store.decrement()}>-</button>
-<button on:click={() => store.increment()}>+</button>
+Count: {$count}
+<button on:click={count.decrement}>-</button>
+<button on:click={count.increment}>+</button>

c77ac6af615e01870d40fe7150942f5a128f40c6
Joshua Nussbaum <joshnuss@gmail.com>
2021-12-18 07:29:20 -0500

    Update Counter.svelte

--- use-state/svelte/src/Counter.svelte
+++ use-state/svelte/src/Counter.svelte
@@ -4,5 +4,5 @@
 
 Count: {count}
 <button on:click={() => count = 0}>Reset</button>
-<button on:click={() => count -= 1}>-</button>
-<button on:click={() => count += 1}>+</button>
+<button on:click={() => count-- }>-</button>
+<button on:click={() => count++ }>+</button>
